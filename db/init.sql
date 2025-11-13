874a8f47f380b266f7ec20d13ce0d47fa80dadb9
root <root@test.myguest.virtualbox.org>
2025-11-13 18:34:07 +0530

    update

--- db/init.sql
+++ db/init.sql
@@ -1,18 +1,17 @@
-
-
-
-
-
-
-
-
-
-
-
-
 CREATE DATABASE IF NOT EXISTS school;
 USE school;
 
-CREATE TABLE IF NOT EXISTS student (id INT AUTO_INCREMENT PRIMARY KEY, name varchar(40), roll_number int, class varchar(16));
+CREATE TABLE IF NOT EXISTS student (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40),
+    roll_number INT,
+    class VARCHAR(16)
+);
 
-CREATE TABLE IF NOT EXISTS teacher (id INT AUTO_INCREMENT PRIMARY KEY, name varchar(40), subject varchar(40), class varchar(16));
+CREATE TABLE IF NOT EXISTS teacher (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(40),
+    subject VARCHAR(40),
+    class VARCHAR(16)
+);
+
